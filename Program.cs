58aea33ad53b87a907444c74be0943efcba1f5fe
ingMayra <Esmedbalan@gmail.com>
2022-11-11 01:35:27 -0600

    codigo del BotApi de telegram

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Telegram.BotAPI;
+using Telegram.BotAPI.GettingUpdates;
+using Telegram.BotAPI.AvailableMethods;
+namespace Bot
+{
+    class program
+    {
+    static void Main(string[] args)
+    
+    {
+        var bot = new BotClient("5743894715:AAH8UWLxPsMF5v3A1GEqjTemts8nLZUev0I");
+        var updates = bot.GetUpdates();
+        while(true){
+            if(updates.Length >0){
+                Console.WriteLine("hay updates");
+                foreach(var update in updates){
+                    if (update.Message is not null){
+                        var Message = update.Message;
+                        bot.SendMessage(Message.Chat.Id, "hola");
+                        Console.WriteLine(update.Message.Text);
+
+                    }
+                }
+                updates= bot.GetUpdates(updates.Max(u => u.UpdateId) + 1);
+            }else{
+                updates = bot.GetUpdates();
+                
+            }
+        }
+      
+    }
+        
+ }
+}
+
+
+
+
